7e414ffbf1d25302c794e709729eea35b6b82cdc
SL-GW <162018972+SL-GW@users.noreply.github.com>
2024-05-01 12:11:08 +0200

    Added functionality for the select

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 setInterval(function () {
   let losAngelesElement = document.querySelector("#los-angeles");
+
   let losAngelesDateElement = losAngelesElement.querySelector(".date");
   let losAngelesTimeElement = losAngelesElement.querySelector(".time");
   losAngelesDateElement.innerHTML = moment().format("MMMM Do YYYY");
@@ -25,3 +26,22 @@ setInterval(function () {
   madridDateElement.innerHTML = moment().format("MMMM Do YYYY");
   madridTimeElement.innerHTML = moment().tz("Europe/Madrid").format("HH:mm:ss");
 }, 1000);
+
+function updateCity(event) {
+  let cityTimeZone = event.target.value;
+  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+  let cityTime = moment().tz(cityTimeZone);
+  let citiesElement = document.querySelector("#cities");
+  citiesElement.innerHTML = `
+   <div class="city">
+          <div class="left">
+            <h2>${cityName}</h2>
+            <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
+          </div>
+          <div class="time">${cityTime.format("HH:mm:ss")}</div>
+        </div>
+  `;
+}
+
+let citiesSelectElement = document.querySelector("#city");
+citiesSelectElement.addEventListener("change", updateCity);
