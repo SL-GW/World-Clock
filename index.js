d1e206cdd6b55c8cb2a4ae51545e27091db92699
SL-GW <162018972+SL-GW@users.noreply.github.com>
2024-05-01 09:53:59 +0200

    Added JS to update times and dates for cities

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
+setInterval(function () {
+  let losAngelesElement = document.querySelector("#los-angeles");
+  let losAngelesDateElement = losAngelesElement.querySelector(".date");
+  let losAngelesTimeElement = losAngelesElement.querySelector(".time");
+  losAngelesDateElement.innerHTML = moment().format("MMMM Do YYYY");
+  losAngelesTimeElement.innerHTML = moment()
+    .tz("America/Los_Angeles")
+    .format("HH:mm:ss");
+}, 1000);
 
+setInterval(function () {
+  let sydneyElement = document.querySelector("#sydney");
+  let sydneyDateElement = sydneyElement.querySelector(".date");
+  let sydneyTimeElement = sydneyElement.querySelector(".time");
+  sydneyDateElement.innerHTML = moment().format("MMMM Do YYYY");
+  sydneyTimeElement.innerHTML = moment()
+    .tz("Australia/Sydney")
+    .format("HH:mm:ss");
+}, 1000);
+
+setInterval(function () {
+  let madridElement = document.querySelector("#madrid");
+  let madridDateElement = madridElement.querySelector(".date");
+  let madridTimeElement = madridElement.querySelector(".time");
+  madridDateElement.innerHTML = moment().format("MMMM Do YYYY");
+  madridTimeElement.innerHTML = moment().tz("Europe/Madrid").format("HH:mm:ss");
+}, 1000);
